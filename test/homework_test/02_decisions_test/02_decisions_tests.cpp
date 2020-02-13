f0e038c355ca36caeac78e2b27934f725c880226
Ruben Yanez <burning.ruby1909@gmail.com>
2020-02-13 16:34:55 -0600

    HW changes

--- test/homework_test/02_decisions_test/02_decisions_tests.cpp
+++ test/homework_test/02_decisions_test/02_decisions_tests.cpp
@@ -1,7 +1,18 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "decisions.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test get grade points")
+{
+
+}
+
+TEST_CASE("Test calculate GPA")
+{
+
+}
