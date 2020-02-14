d387ddd29e7612b61b5f9e2002fc38586594aad9
Ruby1909 <ruben.yanez@g.austincc.edu>
2020-02-13 19:37:03 -0600

    attempt at Assignment

--- src/classwork/03_assign/loops.cpp
+++ src/classwork/03_assign/loops.cpp
@@ -1,3 +1,5 @@
+#include "loops.h"
+#include<iostream>
 /*
 WITH LOOP OF YOUR CHOICE:
 Write code for function factorial that accepts an int num
@@ -10,3 +12,9 @@ returns 120
 
 DON'T FORGET TO WRITE TEST CASE.  See file loop_test.cpp
 */
+int factorial(int num)
+{
+	int Total = 1;
+	
+	return 0; 
+}
